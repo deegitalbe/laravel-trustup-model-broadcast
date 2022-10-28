00ba42366c9debc77fd98771a2ef10aed115c5db
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-10-28 18:01:17 +0200

    🐛 Naming consistancy.

--- src/Providers/LaravelTrustupModelBroadcastServiceProvider.php
+++ src/Providers/LaravelTrustupModelBroadcastServiceProvider.php
@@ -17,15 +17,15 @@ class LaravelTrustupModelBroadcastServiceProvider extends VersionablePackageServ
         config([
             'broadcasting.connections.trustup' => [
                 'driver' => 'pusher',
-                'key' => env('PUSHER_TRUSTUP_APP_KEY', '$2y$10$svQIJ8lZEqclI9AqcZEBZeKEqhePKWoFTFU85ML2u9w4aa2wbRHG'),
-                'secret' => env('PUSHER_TRUSTUP_APP_SECRET', 'app-secret'),
-                'app_id' => env('PUSHER_TRUSTUP_APP_ID', 'websocket_trustup_io'),
+                'key' => env('TRUSTUP_MODEL_BROADCAST_PUSHER_APP_KEY', '$2y$10$svQIJ8lZEqclI9AqcZEBZeKEqhePKWoFTFU85ML2u9w4aa2wbRHG'),
+                'secret' => env('TRUSTUP_MODEL_BROADCAST_PUSHER_APP_SECRET', 'app-secret'),
+                'app_id' => env('TRUSTUP_MODEL_BROADCAST_PUSHER_APP_ID', 'websocket_trustup_io'),
                 'options' => [
-                    'host' => env('PUSHER_TRUSTUP_HOST', 'websocket.trustup.io'),
-                    'port' => env('PUSHER_TRUSTUP_PORT', 443),
-                    'scheme' => env('PUSHER_TRUSTUP_SCHEME', 'https'),
+                    'host' => env('TRUSTUP_MODEL_BROADCAST_PUSHER_HOST', 'websocket.trustup.io'),
+                    'port' => env('TRUSTUP_MODEL_BROADCAST_PUSHER_PORT', 443),
+                    'scheme' => env('TRUSTUP_MODEL_BROADCAST_PUSHER_SCHEME', 'https'),
                     'encrypted' => true,
-                    'useTLS' => env('PUSHER_TRUSTUP_SCHEME', 'https') === 'https',
+                    'useTLS' => env('TRUSTUP_MODEL_BROADCAST_PUSHER_SCHEME', 'https') === 'https',
                 ]
             ]
         ]);
